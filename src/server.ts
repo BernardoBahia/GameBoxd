ba38d439e11489745bd33e902db54dcf6f83e673
Bernardo Bahia <bernardobebahia@gmail.com>
2025-10-21 16:29:00 -0300

    feat(server): registra rotas de autenticação no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
+import { authRoutes } from "./routes/auth.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", userRoutes);
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
