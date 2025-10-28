5a23577b1fdf272601eddbf6a767d2b0a0bf7d8c
Bernardo Bahia <bernardobebahia@gmail.com>
2025-10-28 19:22:01 -0300

    Registra rotas de game no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import userRoutes from "./routes/user.routes";
 import { authRoutes } from "./routes/auth.routes";
+import gameRoutes from "./routes/game.routes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api/auth", authRoutes);
+app.use("/api", gameRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
