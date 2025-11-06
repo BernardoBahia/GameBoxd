649426762592253a5b34c612317ce6b5d0fc4396
Bernardo Bahia <bernardobebahia@gmail.com>
2025-11-06 19:36:47 -0300

    feat(server): registra rotas de list no servidor

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import userRoutes from "./routes/user.routes";
 import { authRoutes } from "./routes/auth.routes";
 import gameRoutes from "./routes/game.routes";
+import listRoutes from "./routes/list.routes";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api", gameRoutes);
+app.use("/api", listRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
